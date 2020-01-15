99d65977a614e398d61be6071491bf31d27dc3c5
Emil Frisk <emilnygaardfrisk@gmail.com>
2020-01-15 03:16:51 +0100

    intialized

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+	entry: './src/index.tsx',
+	output: {
+		path: path.join(__dirname, '/dist'),
+		filename: 'index_bundle.js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+	module: {
+		rules: [
+			{
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+		]
+	},
+	plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
+}
